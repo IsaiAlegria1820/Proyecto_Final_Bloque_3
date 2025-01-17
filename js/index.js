f4f48bafd1f9665ae8057ac53d7a96008534dc3d
Ivan Alegria <iiaj1005@gmail.com>
2025-01-16 20:26:42 -0600

    lets fuckin gooooo me cago en todo, era la puta etiqueta de form, mandaba el boton como un sumbit y me reiniciaba todo waaaaa

--- js/index.js
+++ js/index.js
@@ -1,23 +1,5 @@
  recetas = [];
 
-//Creando funcion para agregar recetas
-function agregarReceta(titulo, ingredientes, instrucciones, categoria, foto) {
-   //el if verifica si la receta ya existe
-    if(recetas.some(receta => receta.titulo === titulo)){
-        alert("La receta ya existe");
-   }else{
-    let  receta = { //se crea el objeto receta para una facil manipulacion de sus caracteristicas
-        titulo,
-        ingredientes,
-        instrucciones,
-        categoria,
-        foto
-    };    
-    recetas.push(receta); //se agrega la receta a la lista
-    mostrarRecetas(); //mando a llamar la renderizacion cada que se agregue 
-    console.log(recetas); //comprobando que si se haya añadido a la lista XD
-   }
-}
 
 document.getElementById("agregar-receta").addEventListener("submit", function (event) { //escuchando evento para guardar la recetea
     event.preventDefault();
@@ -30,6 +12,25 @@ document.getElementById("agregar-receta").addEventListener("submit", function (e
     agregarReceta(titulo, ingredientes, instrucciones, categoria, foto);
     form.reset(); // limpiamos el formulario por motivos esteticos aun cuando no se agrega la receta
 });
+
+//Creando funcion para agregar recetas
+function agregarReceta(titulo, ingredientes, instrucciones, categoria, foto) {
+    //el if verifica si la receta ya existe
+     if(recetas.some(receta => receta.titulo === titulo)){
+         alert("La receta ya existe");
+    }else{
+     let  receta = { //se crea el objeto receta para una facil manipulacion de sus caracteristicas
+         titulo,
+         ingredientes,
+         instrucciones,
+         categoria,
+         foto
+     };    
+     recetas.push(receta); //se agrega la receta a la lista
+     mostrarRecetas(); //mando a llamar la renderizacion cada que se agregue 
+     console.log(recetas); //comprobando que si se haya añadido a la lista XD
+    }
+ }
 
 //renderizamos o dibujamos las recetas 
 function mostrarRecetas() {
@@ -48,7 +49,7 @@ function mostrarRecetas() {
                 <p>${receta.instrucciones}</p>
                 <p>${receta.categoria}</p>
             </div>
-            <button onclick="eliminarReceta(${receta.id})" class="btn p-sm">Eliminar</button>
+            <button onclick="eliminarReceta('${receta.titulo}')" class="btn p-sm">Eliminar</button>
         </div>
         `;
         listaRecetas.appendChild(elementoLista);
@@ -58,14 +59,40 @@ function mostrarRecetas() {
 //se elimina la receta
 function eliminarReceta(id){
     for (let i = 0; i < recetas.length; i++) {
-        if(recetas[i].id===id){
+        if(recetas[i].titulo===id){
             recetas.splice(i ,1);
             mostrarRecetas();
         }
     }     
 }   
 
-
+function buscarReceta() {
+    //se genera la busqueda
+    const busqueda = document.getElementById("busqueda").value.toLowerCase();//con el toLowerCase() hacemos que la busqueda no sea sensible a mayusculas y minusculas
+    const resultadoBusqueda = recetas.filter(receta => receta.titulo.toLowerCase().includes(busqueda)); // con el toLowerCase permite que la busqueda no sea sensible a mayusculas y minusculas
+    
+    //se renderiza la busqueda
+    const listaRecetasEncontradas = document.getElementById("recetas-encontradas");
+    listaRecetasEncontradas.innerHTML = "";
+    resultadoBusqueda.forEach(receta => {
+        const elementoLista = document.createElement("li");
+        elementoLista.innerHTML = `
+        <div class="container card p-sm">
+            <img src="${receta.foto}" alt="...">
+            <div>
+                <h5>${receta.titulo}</h5>
+                <p>${receta.ingredientes}</p>
+                <p>${receta.instrucciones}</p>
+                <p>${receta.categoria}</p>
+            </div>
+        </div>
+        `;
+        listaRecetasEncontradas.appendChild(elementoLista);
+        //limpiamos el input de busqueda
+        //const formBusqueda=document.getElementById("buscar-receta");
+        //formBusqueda.reset(); 
+    });
+}
 
 //funcion creaada para que cuando se de click a inicio se borre los elementos buscados
 function inicio(){
